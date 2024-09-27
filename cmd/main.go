6c0bc01dd9d55eea42e10d7517423bc6d3f1b180
Andrew <zaharowanrew@gmail.com>
2024-09-27 18:15:14 +0400

    Add: handler signatures

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 
 	project "github.com/Chazarov/rest-app"
+	"github.com/Chazarov/rest-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(project.Server)
-	if err := srv.Run("8880"); err != nil {
+	if err := srv.Run("8880", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
