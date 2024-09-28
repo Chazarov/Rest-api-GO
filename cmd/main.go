e26ef0e26ba0bc2fb72b114fead06c603b342ab3
Andrew <zaharowanrew@gmail.com>
2024-09-28 17:35:50 +0400

    Add: Configuration
    - Added configuration file configs.yaml.
    - Installed library for working with configuration of iml format: Viper

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,27 @@ import (
 	"github.com/Chazarov/rest-app/pkg/handler"
 	"github.com/Chazarov/rest-app/pkg/repository"
 	"github.com/Chazarov/rest-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(project.Server)
-	if err := srv.Run("8880", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil { /// Check GetString("port")!!!
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
