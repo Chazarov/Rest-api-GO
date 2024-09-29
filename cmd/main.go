ef0ce27e81cdbd48a4a7fbb14a2e78a7782eba2a
Andrew <zaharowanrew@gmail.com>
2024-09-29 18:44:14 +0400

    Add: db connection
    - Downloaded module sqlx
    - Added connection to database via sqlx

--- cmd/main.go
+++ cmd/main.go
@@ -18,16 +18,29 @@ import (
 
 func main() {
 	if err := initConfig(); err != nil {
-		log.Fatalf("error initializing configs: %s", err.Error())
+		log.Fatalf("!!! error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		log.Fatalf("!!! failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(project.Server)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil { /// Check GetString("port")!!!
-		log.Fatalf("error initializing configs: %s", err.Error())
+		log.Fatalf("!!! error occured while running http server: %s", err.Error())
 	}
 
 }
